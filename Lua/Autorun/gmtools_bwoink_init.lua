16001f293d30c9017aedf221e0b512db09d4bf8d
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2026-02-02 17:44:51 +0600

    update api

--- Lua/Autorun/gmtools_bwoink_init.lua
+++ Lua/Autorun/gmtools_bwoink_init.lua
@@ -1,28 +1,28 @@
 if SERVER then
+	local addons = require("GMT_Scripts.addons")
+
 	local path = table.pack(...)[1]
 	
 	local function loadAddon()
 		-- Hook to admin PM event
 		Hook.Add("gmtools.adminpm.sent", "gmt_bwoink_event", function(sender, target, message)
-			local bwoink_chr = target.Character
+			local bwoinkCharacter = target.Character
 		
 			-- It will works only if player controls a character.
-			if bwoink_chr ~= nil and bwoink_chr.IsDead == false then
-				local bwoinkAff = AfflictionPrefab.Prefabs["gmtbwoink"]
-				bwoink_chr.CharacterHealth.ApplyAffliction(bwoink_chr.AnimController.MainLimb, bwoinkAff.Instantiate(1))
+			if bwoinkCharacter ~= nil and bwoinkCharacter.IsDead == false then
+				local bwoinkAffliction = AfflictionPrefab.Prefabs["gmtbwoink"]
+				bwoinkCharacter.CharacterHealth.ApplyAffliction(bwoinkCharacter.AnimController.MainLimb, bwoinkAffliction.Instantiate(1))
 			end
 		end)
 	end
-	
+
 	-- Hook on GMTools load
-	Hook.Add("gmtools.loaded", "gmt_bwoink_load", function(sender, target, message)
+	Hook.Add("gmtools.loaded", "gmt_bwoink_load", function(contentPackage, forcedLaunch)
 		for i, mod in ipairs(Game.GetEnabledContentPackages()) do
-			if path.."/filelist.xml" == mod.Path then
-				-- Load only if addon was approved
-				if GMT.API.RegisterAddon(mod) then
-					loadAddon()
-					break
-				end
+			-- Load only if registration was approved
+			if path.."/filelist.xml" == mod.Path and addons.RegisterAddon(mod) then
+				loadAddon()
+				break
 			end
 		end
 	end)
